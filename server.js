61d769ed53cfe83e6c2dd574335760dc081afd7f
hilaryyates <hilryyates@gmail.com>
2022-11-29 19:13:23 -0500

    sign in endpoint working

--- server.js
+++ server.js
@@ -51,10 +51,26 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  // db.select("email", "hash")
-  //   .from("login")
-  //   .where("email", "=", req.body.email)
-  //   .then();
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      console.log(data);
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      console.log(isValid);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("login")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            console.log(user);
+            res.json(user[0]);
+          });
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    });
 });
 
 app.listen(3000, () => console.log("server listening"));
